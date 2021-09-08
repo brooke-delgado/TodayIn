a1b04cdddc708c7a618f1042858a636c84eca3f7
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-09-08 11:04:55 -0500

    getting api keys set up

---
+++ assests/script.js
@@ -0,0 +1,9 @@
+
+
+//API Key variable
+var apiKey = "4f35a3a103e0610b506a03bd9f24a683"
+
+//Current weather condtions Variable
+var srcCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&Appid=" + apiKey + "&units=imperial";
+//5 day forecast variable
+var srcFiveday = "http://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&Appid=" + apiKey + "&units=imperial";
