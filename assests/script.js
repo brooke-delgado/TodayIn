c84e4e01c47f14a25785feb4fbe41f63857b4c66
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-09-08 22:10:46 -0500

    adding javascript to populate weather

--- assests/script.js
+++ assests/script.js
@@ -1,9 +1,29 @@
-
-
 //API Key variable
 var apiKey = "4f35a3a103e0610b506a03bd9f24a683"
 
 //Current weather condtions Variable
-var srcCurrent = "http://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&Appid=" + apiKey + "&units=imperial";
+var currentWeather = "http://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&Appid=" + apiKey + "&units=imperial";
 //5 day forecast variable
-var srcFiveday = "http://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&Appid=" + apiKey + "&units=imperial";
+var weatherFiveday = "http://api.openweathermap.org/data/2.5/weather?q=" + searchInput +"&Appid=" + apiKey + "&units=imperial";
+
+//DOM
+
+var inputEl = document.querySelector(".input");
+var BtnsubmitEl = document.querySelector(".submit");
+var cityHistoryEl = document.querySelector(".history-city");
+var cityName = localStorage.getItem("storeCityname");
+
+//current weather conditions
+$.ajax ({
+    url: currentWeather,
+    method: "GET"
+})
+.then(function(response) {
+    $('.city').html("<h3>" + response.name + "</h3>");
+    $(".weather-icon").html("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' >");
+    $(".temperature").text("Temperature: " + response.main.temp + "F");
+    $(".humidity").text("humidity: " + response.main.humidity + "%");
+    $(".wind").text("Wind Speed: " + response.wind.speed + "MPH");
+    
+})
+
